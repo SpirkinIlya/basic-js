7934c67bbadcbf35c1e3906f7a61d90c417fba22
Ilya Spirkin <spirkinilya@gmail.com>
2023-12-19 19:08:37 +0300

    feat: carbon-dating

--- src/carbon-dating.js
+++ src/carbon-dating.js
@@ -17,8 +17,17 @@ const HALF_LIFE_PERIOD = 5730;
  * dateSample('WOOT!') => false
  *
  */
-function dateSample(/* sampleActivity */) {
-  throw new NotImplementedError('Not implemented');
+function dateSample(sampleActivity) {
+  /*check type of input data*/
+  if (typeof sampleActivity != "string") {
+    return false;
+  }
+  /*check if number and between 0 and 15*/ 
+  const sampleActivityNumber = Number(sampleActivity);
+  if (sampleActivityNumber && sampleActivityNumber < MODERN_ACTIVITY && sampleActivityNumber > 0) {
+    return Math.ceil(Math.log2(MODERN_ACTIVITY/sampleActivityNumber)*HALF_LIFE_PERIOD);
+  }
+  return false;
   // remove line with error and write your code here
 }
 
