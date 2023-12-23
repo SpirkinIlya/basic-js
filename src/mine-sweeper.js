76b69dde9bc7b375693306d16ec0573f59855c31
Ilya Spirkin <spirkinilya@gmail.com>
2023-12-23 23:14:22 +0300

    feat: mine-sweeper

--- src/mine-sweeper.js
+++ src/mine-sweeper.js
@@ -23,10 +23,79 @@ const { NotImplementedError } = require('../extensions/index.js');
  *  [1, 1, 1]
  * ]
  */
-function minesweeper(/* matrix */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+
+function minesweeper(matrix) {
+  const field = [];
+  for (let i = 0; i < matrix.length; i += 1){
+    const matrixRow = matrix[i];
+    let fieldRow = [];
+    
+    for (let j = 0; j < matrixRow.length; j += 1) {
+      let mineCounter = 0;
+      try {
+        if (matrix[i-1][j]) {
+          mineCounter += 1;
+        }
+      } catch (error){
+        
+      }
+      try {
+        if (matrix[i+1][j]) {
+          mineCounter += 1;
+        }
+      } catch (error){
+        
+      }
+      try {
+        if (matrix[i][j+1]) {
+          mineCounter += 1;
+        }
+      } catch (error){
+        
+      }
+      try {
+        if (matrix[i][j-1]) {
+          mineCounter += 1;
+        }
+      } catch (error){
+        
+      }
+      try {
+        if (matrix[i-1][j-1]) {
+          mineCounter += 1;
+        }
+      } catch (error){
+        
+      }
+      try {
+        if (matrix[i+1][j+1]) {
+          mineCounter += 1;
+        }
+      } catch (error){
+        
+      }
+      try {
+        if (matrix[i+1][j-1]) {
+          mineCounter += 1;
+        }
+      } catch (error){
+        
+      }
+      try {
+        if (matrix[i-1][j+1]) {
+          mineCounter += 1;
+        }
+      } catch (error){
+        
+      }
+      fieldRow.push(mineCounter);
+    }
+    field.push(fieldRow);
+  }
+  return field;
 }
+// remove line with error and write your code here
+
 
 module.exports = {
   minesweeper
